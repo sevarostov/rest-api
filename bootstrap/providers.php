ee307d90664a2142d9de1dd10c4c0b6fe59de089
sevarostov <seva_rostov@mail.ru>
2025-01-09 18:42:35 +0300

    rm redundant

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\FortifyServiceProvider::class,
+    #App\Providers\FortifyServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
 ];
